70ccca5c2da11d015b6312f00d021e448b5f0d44
Mandringo <mandringo.duregon@gmail.com>
2025-07-11 23:32:24 +0200

    test libraries

---
+++ R/zzz_imports.R
@@ -0,0 +1,5 @@
+#' @import R.matlab dplyr purrr tidyr tibble readr stringr
+#' @import jsonlite yaml xml2 glue foreach doParallel sensobol magrittr
+#' @import ggplot2 patchwork scales rlang parallel
+NULL
+
